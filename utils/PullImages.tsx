330bd76ac20f731d88b3bc7b5c597c7888096177
Ben Malberg <bmalberg2020@gmail.com>
2023-08-16 15:17:48 -0700

    reinstalled dependencies

--- utils/PullImages.tsx
+++ utils/PullImages.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 const PullImages = async (imageArray: string[]) => {
     const selectedImages = await getImages();
     const matchedImages = imageArray.map((slugName) => {
+        console.log("imageObjects: ", selectedImages);
         return selectedImages.filter(
             (imageObject) => imageObject.slug === slugName
         )[0];
     });
+    console.log("matchedimages: ", matchedImages);
     return matchedImages.map((item) => {
         return (
             <Image
