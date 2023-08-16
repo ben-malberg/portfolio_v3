152cd61aa7319b52e21479364f860c7a71ba485c
Ben Malberg <bmalberg2020@gmail.com>
2023-08-16 14:28:54 -0700

    media breakpoint adjustments moved bannerToggler to component folder

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -35,16 +35,14 @@ const Banner: React.FC<BannerProps> = ({
         fontWeight: 100,
     };
 
-    const tempContent = <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-
-    if (bannerSelected) {
-        setTimeout(() => {
-          const contentElement = document.querySelector('.bannerContent');
-          if (contentElement) {
-            contentElement.scrollIntoView({ behavior: 'smooth' });
-          }
-        }, 0); // Delay in milliseconds, e.g., 1000ms (1 second)
-      }
+    // if (bannerSelected) {
+    //     setTimeout(() => {
+    //       const contentElement = document.querySelector('.bannerContent');
+    //       if (contentElement) {
+    //         contentElement.scrollIntoView({ behavior: 'smooth' });
+    //       }
+    //     }, 0); // Delay in milliseconds, e.g., 1000ms (1 second)
+    //   }
 
     return (
         <>
