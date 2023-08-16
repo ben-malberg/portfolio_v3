7b1be8aec89edd49dabd45a7acd509fdeb0f90da
Ben Malberg <117685534+ben-malberg@users.noreply.github.com>
2023-08-16 11:14:53 -0700

    Update page.tsx
    commneted out unneeded components from app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import AllProjects from "@/components/AllProjects";
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
+// import AllProjects from "@/components/AllProjects";
+// import Navbar from "@/components/Navbar";
+// import Hero from "@/components/Hero";
 import BannerToggler from "@/utils/BannerToggler";
 
 const Home = () => {
